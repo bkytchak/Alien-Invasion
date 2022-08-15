ef697ee0f511b9d505bd7ab4be7fdcfd6fdf1159
Brent A Kytchak <bkytchak@gmail.com>
2022-08-15 15:44:47 -0400

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,14 +25,22 @@ class AlienInvasion:
 
 		while True:
 			#watch for keyboard and mouse events.
-			for event in pygame.event.get():
+			self._check_events()
+			self._update_screen()
+			
+
+			
+
+	def _check_events(self):
+		"""Responds to keypress and mouse events."""
+		for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
 
-			#Redraw the screen during each pass through the loop
+	def _update_screen(self):
+		"""Update images on the screen and flip to a new screen."""
 			self.screen.fill(self.settings.bg_color)
 			self.ship.blitme()
-
 			#Make the most recently drawn screen visible.
 			pygame.display.flip()
 
@@ -40,4 +48,3 @@ if __name__ == '__main__':
 	#Make a game instance and run the game
 	ai = AlienInvasion()
 	ai.run_game()
-
